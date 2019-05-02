ae79467a726b75febedf98bfe04cf903afac720e
xin <1017301964@qq.com>
2019-05-02 22:42:24 +0200

    recover

--- Gardien.cc
+++ Gardien.cc
@@ -1,20 +1,40 @@
 #include "Gardien.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+#define PI 3.14159265
+
+void show_vector(std::vector<int *> v);
+
+
 
 Gardien::Gardien (Labyrinthe* l, const char* modele) : Mover (120, 80, l, modele){
 	isInitPara = false;
+	lab_width = _l -> width();
+	lab_height = _l -> height();
+	iter = 0;
 }
 
 void Gardien::update (void) {
+
 	if(!isInitPara) {
 		initParameters();
 		isInitPara = true;
+		find_direction(get_id_by_x_y(2,10), get_id_by_x_y(5,20));
+		
+		
 	}
+
+
 	if (isDead)
 	{
 		/* code */
 		
 	}
-	else if(!isMeetEnmy) {
+	else {if(!isMeetEnmy) {
+		recover();
 		if(distance > distance_max) {
 			inverse_direction();
 		}
@@ -28,14 +48,15 @@ void Gardien::update (void) {
 		}
 
 		// test if meet enmy
-		Mover * chasseur = _l -> _guards [0];
-		float distanca_with_chasseur = calculate_distance(chasseur-> _x, chasseur -> _y);
+		//Mover * chasseur = _l -> _guards [0];
+		//float distanca_with_chasseur = calculate_distance(chasseur-> _x, chasseur -> _y);
 		/*if(distanca_with_chasseur <= distance_see) {
 			isMeetEnmy = true;
 		}*/
 	} else {
 
 	}
+}
 }
 
 bool Gardien::move (double dx, double dy) {
@@ -74,8 +95,12 @@ void Gardien::initParameters() {
 	index = 0;
 
 	isDead = false;
-	blood = 100;
+	max_blood = 100;
+	current_blood = max_blood;
 	armor = 20;
+
+	recover_interval_time = 5;
+	recover_percent_per_second = 0.05;
 }
 
 bool Gardien::hited(int power, float max_dist, float dist){
@@ -85,17 +110,314 @@ bool Gardien::hited(int power, float max_dist, float dist){
 
 	// left power of the gun
 	int power_left = (int)((1.0 - dist/max_dist)*power);
-	std::cout<< dist << "  " << max_dist << "power lefr : " << power_left << std::endl; 
-	int blood_decreased = power_left - armor;
-	std::cout << "blood_decreased: " << blood_decreased << std::endl;
-	if (blood > blood_decreased) {
-		blood -= blood_decreased;
-		message ("blood left : %d", (int) blood);
+	//std::cout<< dist << "  " << max_dist << "power lefr : " << power_left << std::endl; 
+	int current_blood_decreased = power_left - armor;
+	//std::cout << "current_blood_decreased: " << current_blood_decreased << std::endl;
+	if (current_blood > current_blood_decreased) {
+		current_blood -= current_blood_decreased;
+		time(&last_injured_time);
+		message ("current_blood left : %d", (int) current_blood);
 		return false;
 	} else {
-		blood = 0;
+		current_blood = 0;
 		message ("dead");
 		isDead = true;
 		return true;
 	}
 }
+
+void Gardien::recover(){
+	if (current_blood == max_blood)
+	{
+		return;
+	}
+	time(&current_time);
+	if (current_time - last_recover_time < 1)
+	{
+		return;
+	}
+	if (current_time - last_injured_time >= recover_interval_time)
+	{
+		current_blood += max_blood * recover_percent_per_second;
+		last_recover_time = current_time;
+		cout << "blod  " << current_blood << endl;
+		if(current_blood > max_blood) {
+			current_blood = max_blood;
+		}
+	}
+}
+
+bool Gardien::is_blocked(int start, int end) {
+	int x1 = (int)get_x_by_id(start);
+	int y1 = (int)get_y_by_id(start);
+	int x2 = (int)get_x_by_id(end);
+	int y2 = (int)get_y_by_id(end);
+	if (x2 < x1) {
+		int t = x2;
+		x2 = x1;
+		x1 = t;
+	}
+	if (y2 < y1) {
+		int t = y2;
+		y2 = y1;
+		y1 =t;
+	}
+	for (int i = x1; i <= x2; ++i)
+	{
+		for (int j = y1; j <= y2; ++j)
+		{
+			if (_l -> data(i,j) == 1)
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+int Gardien::find_direction(int start, int end) {
+	int direction = move_angle;
+
+	int DPN = 1;
+
+	cout << "start : " << get_x_by_id(start) << "," << get_y_by_id(start) << endl;
+
+	int HN = get_HN(start, end);
+	int * S = new int[4];
+	S[0] = start;
+	S[1] = start;
+	S[2] = HN;
+	S[3] = 0;
+	std::vector<int *> open_list;
+	std::vector<int *> close_list;
+
+	put(open_list, S);
+
+	while(!open_list.empty()) {
+		int* P = open_list.back();
+		open_list.pop_back();
+		int* Ns = get_points_of_4_direction(P[0]);
+		for (int i = 0; i < 4; ++i)
+		{
+
+			if(!is_accessiabl( Ns[i])) {
+				continue;
+			}
+			if(in_vector(close_list, Ns[i])) {
+				continue;
+			}
+			if(in_vector(open_list, Ns[i])) {
+				int index = get_index_of_element(open_list, Ns[i]);
+				int newGN = P[3] + DPN;
+				int newFN = newGN + get_HN(Ns[i], end);
+
+				if (newFN < open_list[index][2]) {
+					update_vector(open_list, index, newFN, newGN);
+				}
+			} else {
+				int GN = P[3] + DPN;
+				int FN = GN + get_HN(Ns[i], end);
+				int *N = new int[4];
+				N[0] = Ns[i];
+				N[1] = P[0];
+				N[2] = FN;
+				N[3] = GN;
+				put(open_list, N);
+			}
+			
+		}
+		close_list.push_back(P);
+
+		if(P[0] == end) {
+			
+			cout << "=====found====" << endl;
+			//show_vector(close_list);
+			direction = get_route(close_list, start, P[0]);
+			break;
+		}
+	}
+
+	return direction;
+}
+
+// 
+// ============ A* 
+// 
+
+
+
+/**
+ * obtenir les 4 cas au tour de le cas donne
+ * @param  id : la cas donne
+ * @return    unt tableau de id : 4 cas au tour de le cas donne
+ */
+int* Gardien::get_points_of_4_direction(int id){
+	int x = get_x_by_id(id);
+	int y = get_y_by_id(id);
+	int id1 = get_id_by_x_y(x, y-1);
+	int id2 = get_id_by_x_y(x, y+1);
+	int id3 = get_id_by_x_y(x-1, y);
+	int id4 = get_id_by_x_y(x+1, y);
+	int * res = new int[4];
+	res[0] = id1;
+	res[1] = id2;
+	res[2] = id3;
+	res[3] = id4;
+	return res;
+}
+
+/**
+ * push une tableau a la vector pour que le vector decroissant dans l'ordre de la troisième valeur du tableau		
+ * @param open_list : le vector
+ * @param p         : le tableau qui va etre ajoute dans le vector
+ */
+void Gardien::put(std::vector<int *>& open_list, int p[]){
+	bool is_insert = false;
+	for (std::vector<int *>::iterator it = open_list.end(); it != open_list.begin(); --it)
+	{		
+		//cout << " == " << endl;
+		if ( (*(it-1))[2]> p[2] ){
+			open_list.insert(it, p);
+			is_insert = true;
+			break;
+		}
+	}
+	if (!is_insert) {
+		open_list.insert(open_list.begin(), p);
+	}
+}
+
+/**
+ * verifier que si un cas est deja dans le vector
+ * @param  close_list : le vector
+ * @param  id         : id de ce cas
+ * @return            : true si le cas est dans le vector
+ */
+bool Gardien::in_vector(std::vector<int *> close_list, int id)  {
+	int size = close_list.size();
+	for (int i = 0; i < size; ++i)
+	{
+		if(close_list[i][0] == id) {
+			return true;
+		}
+	}
+	return false;
+}
+
+/**
+ * obtenir l'indice de le cas dans le vector
+ * @param  close_list : le vector
+ * @param  id         : id de ce cas
+ * @return            : l'indice
+ */
+int Gardien::get_index_of_element(std::vector<int *> close_list, int id){
+	int size = close_list.size();
+	for (int i = 0; i < size; ++i)
+	{
+		if(close_list[i][0] == id) {
+			return i;
+		}
+	}
+	return -1;
+}
+/**
+ * d'abord, motifier le cas de index par newFN et newGN,
+ * ensuite, trier le vector pour que le vector decroissant dans l'ordre de la troisième valeur du tableau	
+ * @param open_list : le vector
+ * @param index     : l'indice du cas dans le vector on veux motifier
+ * @param newFN     : le valeur de le troisieme cas du tableau 
+ * @param newGN     : le valeur de le quatieme cas du tableau 
+ */
+void Gardien::update_vector(std::vector<int *>& open_list, int index, int newFN, int newGN) {
+	open_list[index][2] = newFN;
+	open_list[index][3] = newGN;
+	int i = index+1;
+	int size = open_list.size();
+	while(i < size) {
+		if (newFN < open_list[i][2]){
+			int * temp = open_list[i];
+			open_list[i] = open_list[index];
+			open_list[index] = temp;
+			index = i;
+			++i;
+		} else {
+			break;
+		}
+	}
+}
+
+int Gardien::get_route(std::vector<int *> close_list, int start, int id) {
+	std::vector<int> route;
+
+	//show_vector(close_list);
+
+	while(id != start) {
+		for (int i = close_list.size()-1; i >= 0; --i)
+		{
+			if (close_list[i][0] == id)
+			{
+				//cout << "id : " << id << endl;
+				route.push_back(id);
+				id = close_list[i][1];
+				break;
+			}
+		}
+	}
+
+	int size = route.size();
+
+	/*for (int i = size-1; i >= 0; --i)
+	{
+		int x = get_x_by_id(route[i]);
+		int y = get_y_by_id(route[i]);
+		cout << "(" << x << "," << y << ") -> ";
+	}
+	cout << endl;
+	cout << "here=====" << endl;*/
+
+	int critical_point = start;
+
+	for (int i = size-1; i >= 0; --i)
+	{
+		if (!is_blocked(start, route[i]))
+		{
+			critical_point = route[i];
+		} else {
+			break;
+		}
+	}
+	int x = get_x_by_id(critical_point);
+	int y = get_y_by_id(critical_point);
+
+	cout << "the critical point : " << "(" << x << "," << y << ")" << endl;
+
+	return calculate_angle(start, critical_point);
+}
+
+int Gardien::calculate_angle(int start, int end){
+	int x1 = (int)get_x_by_id(start);
+	int y1 = (int)get_y_by_id(start);
+	int x2 = (int)get_x_by_id(end);
+	int y2 = (int)get_y_by_id(end);
+	int x = x2-x1;
+	int y = y2-y1;
+	double result;
+	result = atan2 (y,x) * 180 / PI;
+	return (int) result + 360;
+}
+
+void show_vector(std::vector<int *> v){
+	int size = v.size();
+	for (int i = 0; i < size; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			cout <<v[i][j] << "  "; 
+		}
+		int x = (v[i][0])/92;
+		int y = (v[i][0])%92;
+		cout << "  (" << x << "," << y << ")";
+		cout << endl;
+	}
+}
+
--- Gardien.h
+++ Gardien.h
@@ -7,20 +7,20 @@
 #include <string.h>
 #include <stdlib.h>     /* srand, rand */
 #include <ctime>       /* time */
-
+#include <vector>
 
 class Labyrinthe;
 
 class Gardien : public Mover {
 private:
-	//       				 				-x(direction = pi)
+	//       				 				-x(move_angle = 180)
 	//		  				 				^
 	//	  	  				 				|
 	//        				 				|
-	//  -y(direction = 3pi/2)   <----------------------> y(direction = pi/2)
+	//  -y(move_angle = 270)   <----------------------> y(move_angle = 90)
 	//	      				 				|
 	//	      				 				|
-	//	      				 				x(direction = 0)
+	//	      				 				x(move_angle = 0)
 
 	int move_angle;
 
@@ -47,9 +47,26 @@ private:
 
 	int index;
 
-	int blood;
+	//capital initial de survie currant
+	int current_blood;
+	//capital initial de survie maximal
+	int max_blood;
 
 	int armor;
+
+	// la largeur and longeur du labyrinthe. 
+	int lab_width, lab_height;
+
+	int iter;
+
+	// le temps de derniere fois subir de blessure et le temps currant
+	time_t last_injured_time,current_time;
+	//le personnage reste un certain temps sans subir de blessure
+	time_t recover_interval_time;
+	// le temps de dernier fois de incrementer le capital initial de survie
+	time_t last_recover_time;
+	//le persontage de capital initial de survie est incrémenté par seconde
+	float recover_percent_per_second;
 
 public:
 	
@@ -65,9 +82,84 @@ public:
 	// quand a faire bouger la boule de feu...
 	bool process_fireball (float dx, float dy) { return false; }
 
+	/**
+	 * mettre id pour chaque cas de _l -> _data en utilisant (x,y)
+	 * @param  x : l'indice de ce cas sur la dimension du largeur du labyrinthe
+	 * @param  y : l'indice de ce cas sur la dimension du longeur du labyrinthe
+	 * @return   id de ce cas
+	 */
+	int get_id_by_x_y(int x, int y) {
+		return x*lab_height+y;
+	}
+
+	/**
+	 * obtenir l'indice de le cas sur la dimension du largeur du labyrinthe
+	 * @param  id : id de ce cas
+	 * @return  l'indice de le cas sur la dimension du largeur du labyrinthe
+	 */
+	int get_x_by_id(int id) {
+		return id/lab_height;
+	}
+
+	/**
+	 * obtenir l'indice de le cas sur la dimension du longeur du labyrinthe
+	 * @param  id : id de ce cas
+	 * @return   l'indice de le cas sur la dimension du longeur du labyrinthe
+	 */
+	int get_y_by_id(int id) {
+		return id%lab_height;
+	}
+
+	/**
+	 * obtenir le cout marchant de le cas now a end
+	 * @param  now : le cas courant
+	 * @param  end : le cas de destination
+	 * @return     : le cout
+	 */
+	float get_HN(float now, float end){
+		return abs(get_x_by_id(now) - get_x_by_id(end))+abs(get_y_by_id(now) - get_y_by_id(end));
+	}
+
+	/**
+	 * verifier que s'il exist 1 dans tous les cas dans le minimun rectangle qui contient les cas start et end
+	 * @param  start : id du cas dans _l -> _data
+	 * @param  end   : id du cas dans _l -> _data
+	 * @return   true si il y a au mois un 1, false sinon
+	 */
+	bool is_blocked(int start, int end);
+
+	/**
+	 * verifier que si le cas est 1 ou 0
+	 * @param  id : id de ce cas
+	 * @return   true si le cas est 0, false sinon
+	 */
+	bool is_accessiabl(int id){
+		int x = get_x_by_id(id);
+		int y = get_y_by_id(id);
+		return _l -> data(x,y)  == 0;
+	}
+	int calculate_angle(int start, int end);
+	int* get_points_of_4_direction(int id);
+	bool in_vector(std::vector<int *> close_list, int id);
+	int get_index_of_element(std::vector<int *> close_list, int id);
+	void update_vector(std::vector<int *>& open_list, int index, int newFN, int newGN);
+	int get_route(std::vector<int *> close_list, int start, int id);
+	void put(std::vector<int *>& open_list, int p[]);
+
+	int find_direction(int start, int end);
+
 	void initParameters();
 
+	/**
+	 * the gardien is hited by chasseur
+	 * @param  power    : 
+	 * @param  max_dist [description]
+	 * @param  dist     [description]
+	 * @return          [description]
+	 */
 	bool hited(int power, float max_dist, float dist);
+
+	void recover();
 
 	void inverse_direction(){
 		//direction = my_mod((direction + M_PI), (2*M_PI));
@@ -81,8 +173,8 @@ public:
 		_angle = (move_angle+270) % 360;
 	}
 
-	void change_direction(){
-		move_angle = (move_angle + 45) % 360;
+	void change_direction(int angle){
+		move_angle = angle;
 		_angle = (move_angle+270) % 360;
 	}
 
